3f55a3ef8756f416a03e146bdc887384d829ffd8
ruteee <rute.s.abreu@gmail.com>
2019-09-14 17:24:19 -0300

    Pre setting of detection event script

---
+++ frontend/src/get_event.js
@@ -0,0 +1,17 @@
+// import $ from  'jquery'
+
+console.log("aqui")
+
+// this.$(document).ready(function(){
+//     console.log( document.getElementById("iframe-id"))
+//     document.getElementById("iframe-id").addEventListener("load", function(){
+//         console.log("loaded")
+//         iframe_el = document.getElementById("iframe-id")
+//         var innerDoc = iframe_el.contentWindow.document || iframe_el.contentWindow
+
+//         events = ['click', 'mouseover']       
+//         $.each(events, function(idx, val){
+//             innerDoc.addEventListener(val, e => { console.log(event.target.type)})
+//         });
+//     });
+// });
--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import './Main.css'
 
+class Main extends React.Component{
 
-export default function Main(){
-    return(
-        <div> 
-            <h3> View da configuração</h3>
-            <div className='app-settings'> teste</div>
-            <div className='div-iframe'>                  
-                 <iframe title = "Main iframme" src="https://sigaa.ufrn.br/sigaa/public/home.jsf">
-                     <p> Page could not be loaded</p>
-                 </iframe>
-            </div>
-        </div>
-    )
+   componentDidMount() {
+        console.log('mount')
+        this.script = document.createElement("script");
+        this.script.src = "../src/get_event.js";
+        this.script.async = true;
+        this.script.type="text/jsx"
+        this.script.onload = () => this.onScriptLoad();
+        document.body.appendChild(this.script);
 }
+
+    render(){
+        return(
+
+            <div>
+                <h3> View da configuração</h3>
+                <div className='app-settings'> teste</div>
+                <div className='div-iframe'>                  
+                     <iframe title = "Main iframme" src="http://localhost:8080/t2.html" id='iframe-id'>
+                         <p> Page could not be loaded</p>
+                     </iframe>
+                </div>
+                
+            </div>
+        )};
+}
+
+export default Main
