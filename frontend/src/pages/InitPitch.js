465021dc54540135cf497fdcc5ea6c511a46246f
Yuri Thomas <yuri.tpinheiro@gmail.com>
2019-09-16 20:06:44 -0300

    List pitches add to home
    without css

--- frontend/src/pages/InitPitch.js
+++ frontend/src/pages/InitPitch.js
@@ -1,9 +1,20 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 import './InitPitch.css'
+import api from '../services/api';
 
 function InitPitch({ history }){
 
-const [webpage, setWebPage] = useState('');
+    const [webpage, setWebPage] = useState('');
+    const [ pitches, setPitchList ] = useState( [] );
+
+    useEffect(() => {
+        async function loadPitches() {
+            const response = await api.get('/pitches');
+            setPitchList(response.data);
+        }
+
+        loadPitches();
+    }, []);
 
     function handleSubmit(e){
         e.preventDefault()
@@ -11,15 +22,31 @@ const [webpage, setWebPage] = useState('');
         history.push(`main`, { url_content: webpage });
     }
     return (
-        <div className="init-settings">
-            <form onSubmit={ handleSubmit}>
-                <input type='text' 
-                    placeholder ='Escreva a url da sua aplicação' 
-                    value = { webpage }  
-                    onChange={ e => setWebPage(e.target.value)} />
-                <button type='submit'> Configurar Aplicação</button>
-            </form>
-            
+        <div>
+            <div className="init-settings">
+                <form onSubmit={ handleSubmit}>
+                    <input type='text' 
+                        placeholder ='Escreva a url da sua aplicação' 
+                        value = { webpage }  
+                        onChange={ e => setWebPage(e.target.value)} />
+                    <button type='submit'> Configurar Aplicação</button>
+                </form>
+            </div>
+
+            { pitches.length > 0 ? (
+                <div className="pitch-list">
+                    <ul>
+                        { pitches.map((pitch) => (
+                            <li key={ pitch._id }>
+                                <p>{ pitch.name }</p>
+                                <p>{ pitch.url }</p>
+                            </li>
+                        )) }
+                    </ul>
+                </div>
+            ) : (
+                <div className="empty">Acabou :(</div>
+            ) }
         </div>
     );
 }
