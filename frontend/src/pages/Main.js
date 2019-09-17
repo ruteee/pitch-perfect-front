5bd7bc51661329b6630d3cf2af2b8a38f84eac8c
ruteee <rute.s.abreu@gmail.com>
2019-09-17 01:07:05 -0300

    Mounting of pitch object

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,11 +1,14 @@
 import React, {useCallback} from 'react'
 import './Main.css'
-// import $ from 'jquery'
+import  api from '../services/api'
 
-function Main({ location: { state } }){
+
+function Main({ location: { state } , match}){
 
     const elements = []
     var flag_in = false
+    var capturing = false
+    var pitch= []
     
     const node_iframe = useCallback(node =>{
         if (node !== null){
@@ -19,13 +22,24 @@ function Main({ location: { state } }){
                     return innerDoc.addEventListener(val, e => {
                         e.preventDefault();
                         console.log(e.target.type);
-                        if (e.target.type !== 'undefined')
-                            treat_element(e.target)                    })
+                        if (e.target.type != null && capturing){
+                            treat_element(e.target)        
+                        }
+                    })
                 });
             });
         }
 
     }, [])
+
+    async function sendPitch(id, pitch_obj){
+        await api.post(`/pitch/`, null, {
+            headers: { pitch: match.params.id},
+            data: {
+                picth: pitch_obj}
+        });
+
+    }
 
     function treat_element(element){
         var element_treated = {}
@@ -55,16 +69,32 @@ function Main({ location: { state } }){
         }        
     }
     
+    function mount_pitch_obj(){
+        const uuidv4 = require('uuid/v4')
+        var name_picth =  document.getElementById("pitch_name").value;
+        var pitch_obj = {'id': uuidv4() , 'name': name_picth, 'actions': pitch }
+        console.log("mounted",pitch_obj)
+        console.log('id', pitch_obj.id)
+
+        // sendPitch(pitch_obj.id, pitch_obj)
+    }
 
     function set_action( ){
         console.log(elements)
+        pitch.push(elements)
     }
     return(
         <div> 
             <h3> View da configuração</h3>
             <div className='app-settings'> 
-                <button  className='set-button' onClick= {set_action}> Cadastrar nova ação </button>
-
+                <span> Iniciar modo de captura</span>
+                <label className="switch">
+                    <input onClick ={e => capturing = !capturing} type="checkbox"/>
+                    <span className="slider round"></span>
+                </label>   
+                <input id ="pitch_name" type='text' placeholder = "Digite o nome do seu pitch"/>  
+                <button  id='btn-register' className='set-button' onClick= {set_action}> Cadastrar nova ação </button>
+                <button  id='btn-end-register' className='set-button' onClick={mount_pitch_obj}> Finalizar </button>
             </div>
             <div className='div-iframe'>                  
                  <iframe ref={node_iframe} title = "Main iframme" src={state['url_content']}>
