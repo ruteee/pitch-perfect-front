185b36d38b0098b57b8081e290850dd73a3ba578
ruteee <rute.s.abreu@gmail.com>
2019-09-16 22:56:06 -0300

    Parcial implementation of pitch's register'

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -4,7 +4,10 @@ import './Main.css'
 
 function Main({ location: { state } }){
 
-    console.log(state);
+    const elements = []
+    var element_treated = {}
+    var flag_in = false
+    
     const node_iframe = useCallback(node =>{
         if (node !== null){
             node.addEventListener("load", function(){
@@ -12,20 +15,63 @@ function Main({ location: { state } }){
                 var iframe_el = node
                 var innerDoc = iframe_el.contentWindow.document || iframe_el.contentWindow
 
-                var events = ['click', 'mouseover'] 
+                var events = ['click', 'change'] 
                 events.map(function(val, idx){
-                    return innerDoc.addEventListener(val, e => { console.log(e.target.type)})
+                    return innerDoc.addEventListener(val, e => {
+                        e.preventDefault();
+                        console.log(e.target.type);
+                        if (e.target.type !== 'undefined')
+                            treat_element(e.target);
+                    })
                 });
             });
         }
 
     }, [])
+
+    function treat_element(element){
+        element_treated.type= element.type
+        element_treated.id = element.id
+
+        switch (element.type) {
+            case 'text':
+                element_treated.attr= element.value
+                break;
+            case 'submit':
+                element_treated.attr = 'click'
+                break;
+            default:
+                break;
+        }
+        flag_in = false
+        for (const idx_ in elements) {
+            if (elements[idx_]['id'] === element_treated.id){
+                elements[idx_] = element_treated
+                flag_in = true          
+                console.log("after",elements)      
+            }         
+        }
+
+        if (!flag_in){
+            elements.push(element_treated)
+        }
+        console.log(elements)
+        
+    }
+    
+
+    function set_action( ){
+        console.log(elements)
+    }
     return(
         <div> 
             <h3> View da configuração</h3>
-            <div className='app-settings'> teste</div>
+            <div className='app-settings'> 
+                <button  className='set-button' onClick= {set_action}> Cadastrar nova ação </button>
+
+            </div>
             <div className='div-iframe'>                  
-                 <iframe ref={node_iframe} title = "Main iframme" src="http://localhost:3001/">
+                 <iframe ref={node_iframe} title = "Main iframme" src={state['url_content']}>
                      <p> Page could not be loaded</p>
                  </iframe>
             </div>
