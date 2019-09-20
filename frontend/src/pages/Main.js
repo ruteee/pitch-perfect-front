7f6c4f8f26007ff581914073d6f7d0d139ab2c59
ruteee <rute.s.abreu@gmail.com>
2019-09-19 22:02:17 -0300

    Correction of steps object creation and addition of restrictions to build pitch

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import './Main.css'
 import  api from '../services/api'
 
@@ -7,41 +7,35 @@ function Main({ location: { state }}){
 
     const [actionstate, setActionstate] = useState([]);
     const [capturing, setCapturing] = useState(false);
-    // var capturing = false;
+    // const [flagIn, setFlagIn] = useState(false);
+
+
 
     var actions = []
-    var flag_in = false
+    var flagIn = false
     var steps= []
-    
-    const node_iframe = useCallback(node =>{
-        if (node !== null){
-            node.addEventListener("load", function(){
-                console.log("loaded")
-                var iframe_el = node
-                var innerDoc = iframe_el.contentWindow.document || iframe_el.contentWindow
 
-                var events = ['click', 'change'] 
-                events.map(function(val, idx){
-                    return innerDoc.addEventListener(val, e => {
-                        e.preventDefault();
-                        console.log(e.target.type);
-                        console.log(e.type)
-                        if (e.target.type != null){
-                            build_action(e)        
-                        }
-                    })
-                });
+    const node_iframe = useEffect(()=>{
+        var iframe_el = document.getElementById("iframe-id")
+        var innerDoc = iframe_el.contentWindow.document || iframe_el.contentWindow
+
+        var events = ['click', 'change']
+        
+        if (capturing){
+            events.map(function(val, idx){
+                return innerDoc.addEventListener(val, e => {
+                    e.preventDefault();
+                    console.log(e.target.type);
+                    if (e.target.type != null){
+                        build_action(e)        
+                    }
+                })
             });
         }
+    }, [capturing])
 
-    }, [])
-
-    async function sendPitch(pitch_obj){
-        await api.post(`/pitch/`, pitch_obj);
-
-    }
-
-    function build_action(e){
+      
+    function build_action(e) {
         var action = {}
         action.event = e.type
         action.target = e.target.id
@@ -53,43 +47,43 @@ function Main({ location: { state }}){
             default:
                 break;
         }
-        flag_in = false
+        flagIn = false
         for (const idx_ in actions) {
             if (actions[idx_]['target'] === action.target){
                 actions[idx_] = action
-                flag_in = true             
+                flagIn = true         
             }         
         }
 
-        if (!flag_in)
+        if (!flagIn){
             actions.push(action)
-
-        console.log('ac',actions)
-        // setActionstate(actions)
+        }       
+        // var copy_actions = JSON.parse(JSON.stringify(actions))
+        //  setActionstate(copy_actions)
     }
 
+
+    async function sendPitch(pitch_obj){
+        await api.post(`/pitch/`, pitch_obj);
+    }
     
     function mount_pitch(){       
-        console.log('mounr' ,capturing)
-
-        // if (capturing){
+        if (capturing){
             var name=  document.getElementById("pitch_name").value;
             var pitch = {'name': name, 'url': state['url_content'], 'steps': steps }
             console.log("mounted",pitch)
-            console.log('id', pitch)
             sendPitch( pitch)
-        // }
+            steps = []
+        }
      
     }
-
     function update_steps( ){
-        console.log('uo',capturing, actions)
-        // if (capturing){
+        var copy_actions = JSON.parse(JSON.stringify(actions))
+        if (capturing){
             var trigger = document.getElementById('gatilho').value
-            steps.push({trigger, 'actions': actions})
-            console.log('up',actions)
-
-        // }
+            steps.push({trigger, 'actions': copy_actions})
+        }
+        actions = []
     }
     
 
@@ -99,19 +93,18 @@ function Main({ location: { state }}){
             <span className='lbl-init' > Init record</span>
                 <div className='init'>
                     <label className="switch">
-                        {/* <input id='cap'onClick ={x=> setCapturing(true)} type="checkbox"/> */}
-                        <input id='cap' type="checkbox"/>
+                        <input id='cap' type="checkbox" onClick= { e=> setCapturing(!capturing) }/>
                         <span className="slider round"></span>
                     </label> 
                     <input type='text' className='gatilho' id='gatilho' placeholder='Gatilho' />
                 </div>
 
                 <div className='capt-form'>
-                    <input id ="pitch_name" type='text' placeholder = "Pitch Name"/>  
-                    <button  id='btn-register' className='set-button' onClick= {update_steps}> Add step </button>
-                    <button  id='btn-end-register' className='end-pitch' onClick={mount_pitch}> End  </button>
+                    <input id ="pitch_name" type='text' placeholder = "Pitch Name" />  
+                    <button  id='btn-register' className='set-button' onClick= {update_steps} > Add step </button>
+                    <button  id='btn-end-register' className='end-pitch' onClick={mount_pitch} > End  </button>
 
-                    {/* {actionstate.length > 0 ? (
+                    {actionstate.length > 0 ? (
                     <div>
                         <ul>
                             {actionstate.map((item, i) => (
@@ -132,15 +125,12 @@ function Main({ location: { state }}){
                     <div>
                         <h3> Sem steps  cadastrados </h3>
                     </div>
-                )} */}
+                )}
                 </div>
-
-                
-
-                
+  
             </div>
             <div className='div-iframe'>                  
-                 <iframe ref={node_iframe} title = "Main iframme" src={state['url_content']}>
+                 <iframe ref={node_iframe} id = 'iframe-id' title = "Main iframme" src={state['url_content']}>
                      <p> Page could not be loaded</p>
                  </iframe>
             </div>
