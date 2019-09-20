8e1de03e1f714f702d699e41626bb0e938d71d9a
ruteee <rute.s.abreu@gmail.com>
2019-09-20 17:14:34 -0300

    Styling of main and init pages

--- frontend/src/pages/InitPitch.js
+++ frontend/src/pages/InitPitch.js
@@ -2,8 +2,98 @@ import React, { useState, useEffect } from 'react'
 import './InitPitch.css'
 import api from '../services/api';
 
-function InitPitch({ history }){
+import {Button, TextField, FormGroup,AppBar,Toolbar, Drawer, Typography, IconButton, Divider, List, ListItemText, ListItem, FormControl } from '@material-ui/core/'
+import MenuIcon from '@material-ui/icons/Menu';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import DesktopMacIcon from '@material-ui/icons/DesktopMac';
 
+
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import clsx from 'clsx';
+
+/*bas stuff*/
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+
+  button: {
+    margin: theme.spacing(1),
+  },
+  textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+    },
+  formGroup: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: theme.spacing(),
+    paddingBottom: theme.spacing(30),
+    paddingTop: theme.spacing(10)
+  }
+}));
+
+
+function InitPitch({ history }){        
+    const classes = useStyles();
+    const theme = useTheme();
+
+    const [open, setOpen] = React.useState(false);
     const [webpage, setWebPage] = useState('');
     const [ pitches, setPitchList ] = useState( [] );
 
@@ -16,39 +106,101 @@ function InitPitch({ history }){
         loadPitches();
     }, []);
 
+    function handleDrawerOpen() {
+        setOpen(true);
+      }
+    
+      function handleDrawerClose() {
+        setOpen(false);
+    }
+
     function handleSubmit(e){
         e.preventDefault()
+        console.log("here")
         console.log(webpage)
         history.push(`main`, { url_content: webpage });
     }
     return (
-        <div>
-            <div className="init-settings">
-                <form onSubmit={ handleSubmit}>
-                    <input id= "text_url" type='text' 
-                        placeholder ='Escreva a url da sua aplicação' 
-                        value = { webpage }  
-                        onChange={ e => setWebPage(e.target.value)} />
-                    <button id="btn_send_url" type='submit'> Configurar Aplicação</button>
-                </form>
+    <div id= "main_class" className={classes.root}>
+        <AppBar
+            position="fixed"
+            className={clsx(classes.appBar, {
+            [classes.appBarShift]: open,
+            })}>
+            <Toolbar>
+            <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerOpen}
+                edge="start"
+                className={clsx(classes.menuButton, open && classes.hide)}
+            >
+                <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" noWrap>
+                    Pitch Perfect
+                </Typography>
+                </Toolbar>
+        </AppBar>
+        <Drawer
+            className={classes.drawer}
+            variant="persistent"
+            anchor="left"
+            open={open}
+            classes={{
+            paper: classes.drawerPaper,
+            }}>
+            <div className={classes.drawerHeader}>
+            <IconButton onClick={handleDrawerClose}>
+                {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            </IconButton>
             </div>
+            <Divider />
+                { pitches.length > 0 ? (
+                    <div className="pitch-list">
+                       <List>
+                            { pitches.map((pitch, i) => (
+                                <ListItem button key={i}>
+                                    <ListItemIcon> 
+                                        <DesktopMacIcon/>
+                                    </ListItemIcon>
+                                    <ListItemText key={pitch.id} >
+                                        <p> Nome:  { pitch.name } </p>
+                                        <p> URL: { pitch.url }</p>
+                                    
+                                    </ListItemText>
+                                </ListItem>
+                                
+                                
+                            )) }
+                        </List>
+                    </div>
+                ) : (
+                    <div className="empty">Não existem pitchies cadastrados :(</div>
+                    )}
+            <Divider />
+        </Drawer>
 
-            { pitches.length > 0 ? (
-                <div className="pitch-list">
-                    <ul>
-                        { pitches.map((pitch) => (
-                            <li key={ pitch._id }>
-                                <p>{ pitch.name }</p>
-                                <p>{ pitch.url }</p>
-                            </li>
-                        )) }
-                    </ul>
-                </div>
-            ) : (
-                <div className="empty">Acabou :(</div>
-            ) }
-        </div>
-    );
-}
+        <main
+            className={clsx(classes.content, {
+            [classes.contentShift]: open,
+            })}
+        >
+            <div className="init-settings">
+                <FormGroup  className={classes.formGroup} id='form_main'>
+                    <TextField id= "text_url" type='text' 
+                        label ='Digite a url da sua aplicação' 
+                        value = { webpage }  
+                        onChange={ e => setWebPage(e.target.value)} 
+                        variant='outlined'  
+                        margin="normal"  
+                        className={classes.textField}/>
+                    <Button variant ="contained"  className={classes.Button} id="btn_send_url" onClick={handleSubmit}> Configurar Aplicação</Button>
+                </FormGroup>
+            </div>
+            
+        </main>
+    </div>
+    )};
 
 export default InitPitch;
--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -2,15 +2,51 @@ import React, {useEffect, useState} from 'react'
 import './Main.css'
 import  api from '../services/api'
 
-import {Button, FormGroup, Input} from 'reactstrap'
+import 'typeface-roboto';
+import {Button, TextField, FormGroup, FormControlLabel, FormControl, Switch, AppBar, Toolbar, Typography} from '@material-ui/core/';
+import { makeStyles } from '@material-ui/core/styles';
+import jQuery from  'jquery'
 
 
 function Main({ location: { state }}){
 
-    const [actionstate, setActionstate] = useState([]);
+
+    const useStyles = makeStyles(theme => ({
+        button: {
+          margin: theme.spacing(1),
+        },
+        input: {
+          display: 'none',
+        },
+
+        textField: {
+            marginLeft: theme.spacing(1),
+            marginRight: theme.spacing(1),
+          },
+        FormControlLabel :{
+            marginLeft: theme.spacing(1),
+            marginRight: theme.spacing(1),
+            marginTop: theme.spacing(2)
+        },
+        Switch:{
+         
+        },
+        AppBar:{
+            // height: theme.spacing(6),
+            alignItems: 'center'
+        },
+
+        title: {
+            flexGrow: 1,
+            // marginBottom: theme.spacing(3),
+          },
+      }));
+
+      const classes = useStyles();
+
+    // const [actionstate, setActionstate] = useState([]);
     const [capturing, setCapturing] = useState(false);
     // const [flagIn, setFlagIn] = useState(false);
-
 
 
     var actions = []
@@ -22,25 +58,64 @@ function Main({ location: { state }}){
         var innerDoc = iframe_el.contentWindow.document || iframe_el.contentWindow
 
         var events = ['click', 'change']
+
+        jQuery.fn.extend({
+            getPath: function() {
+                var pathes = [];
+        
+                this.each(function(index, element) {
+                    var path, $node = jQuery(element);
+        
+                    while ($node.length) {
+                        var realNode = $node.get(0), name = realNode.localName;
+                        if (!name) { break; }
+        
+                        name = name.toLowerCase();
+                        var parent = $node.parent();
+                        var sameTagSiblings = parent.children(name);
+        
+                        if (sameTagSiblings.length > 1)
+                        {
+                            var allSiblings = parent.children();
+                            var index = allSiblings.index(realNode) + 1;
+                            if (index > 0) {
+                                name += ':nth-child(' + index + ')';
+                            }
+                        }
+        
+                        path = name + (path ? ' > ' + path : '');
+                        $node = parent;
+                    }
+        
+                    pathes.push(path);
+                });
+        
+                return pathes.join(',');
+            }
+        })
         
         if (capturing){
             events.map(function(val, idx){
                 return innerDoc.addEventListener(val, e => {
                     e.preventDefault();
-                    console.log(e.target.type);
-                    if (e.target.type != null){
+                    console.log(e)
+                    console.log(jQuery(e.target).getPath());
+                    var not_interacts_el = ['DIV', 'P', 'BODY', 'B']
+                    if (!not_interacts_el.includes(e.target.tagName)){
+                        console.log('aqui, ', e.target.tagName)
                         build_action(e)        
                     }
                 })
             });
         }
+        console.log(capturing)
     }, [capturing])
 
-      
     function build_action(e) {
+        var target_element = jQuery(e.target).getPath()
         var action = {}
         action.event = e.type
-        action.target = e.target.id
+        action.target = target_element
 
         switch (e.target.type) {
             case 'text':
@@ -86,25 +161,44 @@ function Main({ location: { state }}){
             steps.push({trigger, 'actions': copy_actions})
         }
         actions = []
+        document.getElementById('gatilho').value=""
     }
     
 
     return(
         <div> 
             <div className='app-settings'>
-            <span className='lbl-init' > Init record</span>
-                <div className='init'>
-                    <label className="switch">
-                        <Input id='cap' type="checkbox" onClick= { e=> setCapturing(!capturing) }/>
+                <AppBar position="static" className={classes.AppBar}>
+                    <Toolbar>
+                        <Typography variant="h6" className={classes.title} >
+                        Confirgurações
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                {/* <div className='init'> */}
+                    {/* <label className="switch">
+                        <input id='cap' type="checkbox" onClick= { e=> setCapturing(!capturing) }/>
                         <span className="slider round"></span>
-                    </label> 
-                    <Input type='text' className='gatilho' size='sm' id='gatilho' placeholder='Gatilho' />
-                </div>
+                    </label>  */}
+                {/* </div> */}
 
-                <FormGroup className='capt-form '>
-                    <Input id ="pitch_name" type='text' placeholder = "Pitch Name" />  
-                    <Button variant ="danger"  id='btn-register' className='set-button btn btn-primary' onClick= {update_steps} > Add step </Button>
-                    <Button variant="danger" id='btn-end-register' className='end-pitch' onClick={mount_pitch} > End  </Button>
+                <FormControl component="fieldset">
+                    <FormGroup className='capt-form '>
+                    <TextField id ="pitch_name" type='text' label= "Pitch Name" variant='outlined'  margin="normal"  className={classes.textField}/> 
+                        <FormControlLabel className={classes.FormControlLabel}
+                                control={
+                                <Switch className={classes.Switch}
+                                    size="medium"
+                                    checked={capturing}
+                                    onChange={e=> setCapturing(!capturing) }
+                                    color="primary"/>
+                                }
+                                label="Iniciar captura de ações"
+                        />
+ 
+                        <TextField type='text' id='gatilho' label='Gatilho'  variant='outlined'  margin="normal"  className={classes.textField} />       
+                        <Button variant ="contained" id='btn-register' className={classes.button} onClick= {update_steps} > Adicionar nova ação</Button>
+                        <Button variant="contained"  id='btn-end-register' className={classes.button} onClick={mount_pitch} > Finalizar  </Button>
 
                     {/* {actionstate.length > 0 ? (
                     <div>
@@ -128,7 +222,8 @@ function Main({ location: { state }}){
                         <h3> Sem steps  cadastrados </h3>
                     </div>
                 )} */}
-                </FormGroup>
+                    </FormGroup>
+                </FormControl>
   
             </div>
             <div className='div-iframe'>                  
