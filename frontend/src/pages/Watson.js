6eee9cd80f9524024fdc7b0920359a61d91109c4
Yuri Thomas <yuri.tpinheiro@gmail.com>
2019-09-20 13:45:03 -0300

    Update watson listener

--- frontend/src/pages/Watson.js
+++ frontend/src/pages/Watson.js
@@ -2,77 +2,32 @@ import React, { useState, useEffect } from 'react';
 
 import api from '../services/api';
 import './Watson.css';
+import recognizeMicrophone from 'watson-speech/speech-to-text/recognize-microphone';
 
+// "watson-speech/speech-to-text/recognize-microphone")
 export default function Watson({ match }) {
-    
-    // var watson_hear =  speech_text();
-
-    // console.log(watson_hear);
 
     return (
         <div className="watson">
-            <p>Olá</p>
-            <button type="button" onClick={ () => speech_text() }>CLick</button>
+            <p id="output">Olá</p>
+            <button type="button" onClick={() => speech_text()}>CLick</button>
         </div>
     );
 
 }
 
-function speech_text() {
-    // const mic = require('mic');
-    const SpeechToTextV1 = require('ibm-watson/speech-to-text/v1');
-    const speechToText = new SpeechToTextV1({
-    iam_apikey: 'Eqky6yJU8L-NTw0XKAmqwVpl3-wgtzRmk0q8jZ6cREgt',
-    url: 'https://stream.watsonplatform.net/speech-to-text/api',
-    model: 'pt-BR_NarrowbandModel',
-    });
+async function speech_text() {
 
-    // 1. Microphone settings
-    // const micInstance = mic({
-    // rate: 44100,
-    // channels: 2,
-    // debug: false,
-    // exitOnSilence: 6
-    // });
+    const token = await api.get('/api/speech-to-text/token');
+    console.log(token.data);
 
-    navigator.getUserMedia = ( navigator.getUserMedia    || navigator.webkitGetUserMedia ||
-      navigator.mozGetUserMedia ||navigator.msGetUserMedia);
+    var textStream = recognizeMicrophone(Object.assign(token.data, { // eslint-disable-line
+        outputElement: '#output' // CSS selector or DOM Element
+    }));
 
+    console.log(textStream);
 
-    // 2. Service recognize settings
-    const recognizeStream = speechToText.recognizeUsingWebSocket({
-    content_type: 'audio/l16; rate=44100; channels=2',
-    model: 'pt-BR_BroadbandModel',
-    interim_results: true,
-    })
-
-    // 3. Start recording
-    // const micInputStream = micInstance.getAudioStream();
-    // micInstance.start();
-    var aCtx;
-    var analyser;
-    var microphone;
-    if (navigator.getUserMedia) {
-    navigator.getUserMedia({audio: true}, function(stream) {
-    aCtx = new AudioContext();
-    analyser = aCtx.createAnalyser();
-    microphone = aCtx.createMediaStreamSource(stream);
-    microphone.connect(analyser);
-    analyser.connect(aCtx.destination);
-    }, function (){console.warn("Error getting audio stream from getUserMedia")});
-    };
-
-    // console.log('Watson is listening, you may speak now.');
-
-    // 4. Pipe audio to service
-    // const textStream = microphone.pipe(recognizeStream).setEncoding('utf8');
-    const textStream = microphone.pipe(recognizeStream).setEncoding('utf8');
-
-
-    // textStream.on('data', user_speech_text => console.log('Watson hears:', user_speech_text));
-    // textStream.on('error', e => console.log(`error: ${e}`));
-    // textStream.on('close', e => console.log(`close: ${e}`));
-
-        
-    return textStream.on('data', user_speech_text => console.log('Watson hears:', user_speech_text));
-  }
+    textStream.on('data', user_speech_text => console.log('Watson hears:', user_speech_text));
+    textStream.on('error', e => console.log(`error: ${e}`));
+    textStream.on('close', e => console.log(`close: ${e}`));
+}
